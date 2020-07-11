6908401079ddcadb4d19a2afddfc899852067cda
Willian Alves Lima <w.alves.lima@gmail.com>
2020-07-11 00:57:32 +0000

    Aula 01

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const servidor  = http.createServer(function (req, resp) {
-    resp.end(`
+    let html = '';
+    if (req.url == '/') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
@@ -10,6 +12,20 @@ const servidor  = http.createServer(function (req, resp) {
                 <h1> Casa do Código </h1>
             </body> 
         </html>
-    `);
+    `;
+    } else if (req.url == '/livros')
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de livros </h1>
+            </body> 
+        </html>
+    `;
+    
+    
+    resp.end(html);
 });
 servidor.listen(3000);
