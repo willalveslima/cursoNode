7ea6f98c58e1bc1009da362586803cafbb4d6278
Willian Alves Lima <w.alves.lima@gmail.com>
2020-07-17 23:09:18 +0000

    Aula 02

--- server.js
+++ server.js
@@ -1,3 +1,39 @@
+const express = require('express')
+const app = express();
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
+
+});
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+
+/*
 const http = require('http');
 
 const servidor  = http.createServer(function (req, resp) {
@@ -29,3 +65,4 @@ const servidor  = http.createServer(function (req, resp) {
     resp.end(html);
 });
 servidor.listen(3000);
+*/
